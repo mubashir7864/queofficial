c74df6d140566bab095ff28b8f647c5ccdcaf240
Mubashir <muchuz7864@gmail.com>
2025-01-30 23:29:15 +0530

    done with creating post

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('home/', views.home , name= 'home'),
     path('', views.home , {}),
     path('signup/', views.signup, name='signup' ),
+    path('createpost/', views.createpost,name='createpost'),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login")
 ]

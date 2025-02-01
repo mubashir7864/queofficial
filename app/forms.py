eb2492d126811f8d872d7c8ea6e3571e00bea7da
Mubashir <muchuz7864@gmail.com>
2025-02-01 17:16:05 +0530

    update post implemented

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Postform
+from .models import Postform, Profile
 
 class Registrationform(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
-    username = forms.CharField(required=True) 
+    username = forms.CharField(required=True)
+
 
     class Meta:
         model = User
@@ -18,3 +19,27 @@ class Post(forms.ModelForm):
     class Meta:
         model = Postform
         fields = ['title', 'description', 'image']
+
+
+
+#update profiledetails
+class ProfiledetailsUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+
+    # Don't include 'username', 'password1', 'password2' for profile update
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']  
+
+
+
+# Update profileimage
+
+class ProfileimageUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ['image']
+
